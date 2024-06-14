7e1aa65bfa14831096ad6aba43e448bffdfcdc8b
Niklas Dießner <40034835+TheBlackSheep3@users.noreply.github.com>
2024-06-14 09:15:46 +0200

    parse from file or stdin

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
+#include <algorithm>
 #include <cstdlib>
 #include <exception>
+#include <fstream>
 #include <iostream>
+#include <iterator>
 #include <ostream>
+#include <utility>
 
 #include "config.hpp"
 #include "parameters.hpp"
@@ -31,11 +35,34 @@ int main(int argc, char const* argv[]) {
     print_version();
     return EXIT_SUCCESS;
   } else {
-    auto genertor = nd::spl::peg::get_spl_parser_generator();
+    std::string parsable_string{};
+    Container<std::ostream> outstream_prt{};
+    if (params.input_file().empty()) {
+      parsable_string =
+          std::string{std::istreambuf_iterator<char>(std::cin), {}};
+    } else {
+      std::ifstream file{params.input_file()};
+      parsable_string = std::string{std::istreambuf_iterator<char>(file), {}};
+    }
+    if (!params.output_requested()) {
+      outstream_prt = std::move(Container<std::ostream>{&std::cout, false});
+    } else {
+      if (!params.output_file().empty()) {
+        outstream_prt = std::move(Container<std::ostream>{
+            new std::ofstream{params.output_file()}, true});
+      } else {
+        std::cerr << "missing parameter OUTPUT_FILE\n\nRun '"
+                  << PROJECT_EXECUTABLE << " " << HELP_LONG_FLAG
+                  << "' for more informatin.\n";
+        return EXIT_FAILURE;
+      }
+    }
+    auto generator = nd::spl::peg::get_spl_parser_generator();
     try {
-      std::cout << genertor.run("x:=1") << std::endl;
-    } catch (std::exception& e) {
+      *outstream_prt << generator.run(parsable_string) << std::endl;
+    } catch (std::exception &e) {
       std::cerr << "Parsing error:\n" << e.what() << std::endl;
+      return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
   }
