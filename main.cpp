50e55d8773464b8a675bd045f556f39f97eb56a9
Niklas Dießner <40034835+TheBlackSheep3@users.noreply.github.com>
2024-07-06 15:03:18 +0200

    add argparse and generade .clangd

--- main.cpp
+++ main.cpp
@@ -1,69 +1,57 @@
-#include <algorithm>
-#include <cstdlib>
-#include <exception>
-#include <fstream>
-#include <iostream>
-#include <iterator>
-#include <ostream>
-#include <utility>
+#include <exception>  // std::exception
+#include <fstream>    // std::ifstream, std::ofstream
+#include <iostream>   // std::cout, std::cerr, std::cin
+#include <ostream>    // std::ostream
+#include <string>     // std::string
+#include <utility>    // std::move
+
+#include <argparse/argparse.hpp>  // argparse::ArgumentParser
 
 #include "config.hpp"
 #include "parameters.hpp"
 #include "spl_generator.hpp"
 
-void print_help() {
-  std::cout << "Usage: " << PROJECT_EXECUTABLE << " [options] [INPUT_FILE]\n";
-  std::cout << "  -o, --output OUTPUT_FILE   Write output to OUTPUT_FILE.\n";
-  std::cout
-      << "  -h, --help                 Print this help message and exit.\n";
-  std::cout << "  -V, --version              Print the version of "
-            << PROJECT_EXECUTABLE << " and exit.\n\n";
-  std::cout << "If no INPUT_FILE is provided, input is read from stdin.\n";
-}
-
-void print_version() {
-  std::cout << PROJECT_EXECUTABLE << " version " << PROJECT_VERSION
-            << std::endl;
-}
-
 int main(int argc, char const* argv[]) {
-  Parameters params{argv + 1, argv + argc};
-  if (params.help_requested()) {
-    print_help();
-    return EXIT_SUCCESS;
-  } else if (params.version_requested()) {
-    print_version();
-    return EXIT_SUCCESS;
-  } else {
-    std::string parsable_string{};
-    Container<std::ostream> outstream_prt{};
-    if (params.input_file().empty()) {
-      parsable_string =
-          std::string{std::istreambuf_iterator<char>(std::cin), {}};
-    } else {
-      std::ifstream file{params.input_file()};
-      parsable_string = std::string{std::istreambuf_iterator<char>(file), {}};
-    }
-    if (!params.output_requested()) {
-      outstream_prt = std::move(Container<std::ostream>{&std::cout, false});
-    } else {
-      if (!params.output_file().empty()) {
-        outstream_prt = std::move(Container<std::ostream>{
-            new std::ofstream{params.output_file()}, true});
-      } else {
-        std::cerr << "missing parameter OUTPUT_FILE\n\nRun '"
-                  << PROJECT_EXECUTABLE << " " << HELP_LONG_FLAG
-                  << "' for more informatin.\n";
-        return EXIT_FAILURE;
-      }
-    }
-    auto generator = nd::spl::peg::get_spl_parser_generator();
-    try {
-      *outstream_prt << generator.run(parsable_string) << std::endl;
-    } catch (std::exception &e) {
-      std::cerr << "Parsing error:\n" << e.what() << std::endl;
-      return EXIT_FAILURE;
-    }
-    return EXIT_SUCCESS;
+  argparse::ArgumentParser program(PROJECT_EXECUTABLE, PROJECT_VERSION);
+  program.add_argument("-o", "--output")
+      .help("write output to OUTPUT_FILE")
+      .metavar("OUTPUT_FILE");
+  program.add_argument("-f", "--file")
+      .help("read input from INPUT_FILE")
+      .metavar("INPUT_FILE");
+  program.add_description(
+      "Parse code of the sample programming language, as described in "
+      "semantics of programming languages, and put out a tree representing the "
+      "program structure.");
+  program.add_epilog("The code is parsed using a PEG parser.");
+
+  try {
+    program.parse_args(argc, argv);
+  } catch (std::exception const& e) {
+    std::cerr << e.what() << "\n\n" << HELP_HINT;
   }
+
+  std::string parsable_string;
+  Container<std::ostream> output_ptr;
+  if (auto input_file = program.present("--file")) {
+    std::ifstream file_stream{*input_file};
+    file_stream >> parsable_string;
+  } else {
+    std::cin >> parsable_string;
+  }
+  if (auto output_file = program.present("--output")) {
+    output_ptr = std::move(
+        Container<std::ostream>{new std::ofstream(*output_file), true});
+  } else {
+    output_ptr = std::move(Container<std::ostream>{&std::cout, false});
+  }
+
+  auto generator = nd::spl::peg::get_spl_parser_generator();
+  try {
+    // *outstream_prt << generator.run(parsable_string) << std::endl;
+  } catch (std::exception& e) {
+    std::cerr << "Parsing error:\n" << e.what() << '\n';
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 }
